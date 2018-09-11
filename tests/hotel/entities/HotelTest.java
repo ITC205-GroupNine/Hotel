d0db4908f909fb0c80f1d378847b720e049ad373
MethodicalMadness <lucifersbeard@hotmail.com>
2018-09-12 01:53:23 +1000

    Setup project structure for unit testing

---
+++ tests/hotel/entities/HotelTest.java
@@ -0,0 +1,24 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelTest {
+
+    @Test
+    void book() {
+    }
+
+    @Test
+    void checkin() {
+    }
+
+    @Test
+    void addServiceCharge() {
+    }
+
+    @Test
+    void checkout() {
+    }
+}
