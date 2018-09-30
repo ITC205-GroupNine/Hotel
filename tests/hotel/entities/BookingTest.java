29e765be3fafc0efa2816bde7c85143a53481880
Emily. C <kauket19@gmail.com>
2018-09-30 19:20:43 +1000

    Fixes for unwanted changes in BookingTest

--- tests/hotel/entities/BookingTest.java
+++ tests/hotel/entities/BookingTest.java
@@ -1,15 +1,11 @@
 package hotel.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import static org.mockito.Mockito.verify;
-
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
-
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -21,12 +17,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import hotel.credit.CreditCard;
-import hotel.credit.CreditCardType;
+import static org.junit.jupiter.api.Assertions.*;
+
+//import org.junit.BeforeClass;
 
 @ExtendWith(MockitoExtension.class)
 class BookingTest {
-
+    
     @Mock Guest testGuest = new Guest("John","20 Pelican Street",04);
     @Mock Room testRoom = new Room(1,RoomType.SINGLE);
     SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
@@ -37,49 +34,49 @@ class BookingTest {
     ServiceType testServiceType;
     double testCost;
     @Spy List<ServiceCharge> charges = new ArrayList<>();
-
+    
     @InjectMocks Booking testBooking = new Booking(testGuest,testRoom,testArrivalDate,testStayLength,testOccupants,testCreditCard);
-
-
+    
+    
     BookingTest() throws ParseException {
     }
-
-
+    
+    
     @BeforeEach
     void setUp()  {
-
+    
     }
-
-
+    
+    
     @Test
     void testCheckInIdealCase() {
         //arrange
-
+        
         //act
         assertTrue(testBooking.isPending());
         testBooking.checkIn();
-
+        
         //assert
-        verify(testRoom).checkIn();
+        //verify(testRoom).checkin();
         assertTrue(testBooking.isCheckedIn());
     }
-
+    
     @Test
     void testCheckInThrowsException() {
         //arrange
-
+        
         //act
         testBooking.checkIn();
-        verify(testRoom).checkIn();
-
+        //verify(testRoom).checkin();
+        
         //assert
         assertTrue(testBooking.isCheckedIn());
         Executable e = () -> testBooking.checkIn();
         Throwable R = assertThrows(RuntimeException.class,e);
         assertEquals("Booking Entity cannot call checkIn except in PENDING State",R.getMessage());
     }
-
-
+    
+    
     @Test
     void addServiceChargeIdealUseCase() {
         //arrange
@@ -87,55 +84,55 @@ class BookingTest {
         assertTrue(testBooking.isCheckedIn());
         testServiceType = ServiceType.BAR_FRIDGE;
         testCost = 20.00;
-
-        //act
+        
+        //act test comment
         testBooking.addServiceCharge(testServiceType,testCost);
-
+        
         //assert
         assertEquals(1,charges.size());
-
+        
     }
-
+    
     @Test
     void addServiceChargeThrowsException() {
         //arrange
         assertFalse(testBooking.isCheckedIn());
         testServiceType = ServiceType.BAR_FRIDGE;
         testCost = 20.00;
-
+        
         //act
         Executable e = () -> testBooking.addServiceCharge(testServiceType,testCost);
         Throwable R = assertThrows(RuntimeException.class,e);
-
+        
         //assert
         assertEquals("Booking Entity charges cannot be added except in CHECKED_IN state",R.getMessage());
-
+        
     }
-
+    
     @Test
     void checkOutIdealUseCase() {
         //arrange
         testBooking.checkIn();
         assertTrue(testBooking.isCheckedIn());
-
+        
         //act
         testBooking.checkOut();
-        verify(testRoom).checkOut(testBooking);
-
-
+        //verify(testRoom).checkout(testBooking);
+        
+        
         //assert
         assertFalse(testBooking.isCheckedIn());
     }
-
+    
     @Test
     void checkOutThrowsException() {
         //arrange
         assertFalse(testBooking.isCheckedIn());
-
+        
         //act
         Executable e = () -> testBooking.checkOut();
         Throwable R = assertThrows(RuntimeException.class,e);
-
+        
         //assert
         assertEquals("Booking Entity charges cannot be added except in CHECKED_IN state",R.getMessage());
     }
